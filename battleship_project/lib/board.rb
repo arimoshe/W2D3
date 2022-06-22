79c6494dfed41ad89f8db332ec7e1cdcffd2ead6
Ari Moshe <arimoshe@gmail.com>
2022-06-21 18:04:28 -0700

    Battleship Almost Done

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,69 @@
 class Board
   
+    attr_reader :size
+
+    def initialize(n)
+        @grid = Array.new(n) {Array.new(n, :N)}
+        @size = n ** 2
+    end
+
+    def [](pos_arr)
+        @grid[pos_arr[0]][pos_arr[1]]
+    end
+
+    def []=(pos_arr, value)
+        @grid[pos_arr[0]][pos_arr[1]] = value
+    end
+
+    def num_ships
+        @grid.flatten.count { |ele| ele == :S}
+    end
+
+    def attack(pos_arr)
+        if self[pos_arr] == :S
+            self[pos_arr] = :H
+            puts "you sunk my battleship!"
+            return true
+        else
+            self[pos_arr] = :X
+            return false
+        end
+    end
+    require "byebug"
+
+    def place_random_ships
+        ship_poses=[]
+
+        while ship_poses.length < (@size / 4 )
+            randA = (rand(0...@grid.length))
+            randB = (rand(0...@grid.length))
+            if !ship_poses.include?([randA,randB])
+                ship_poses << [randA,randB]
+            end
+        end
+        ship_poses.each do |position| 
+            @grid[position[0]][position[1]] = :S 
+        end
+
+    end
+
+    def hidden_ships_grid 
+        @grid.map.with_index { |ele, i| ele.map.with_index { |ele2, j| ele2 == :S ? :N : ele2   }}
+    end
+
+    def self.print_grid(grid)
+        grid.map { |ele| puts ele.join(" ")  }
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(self.hidden_ships_grid)
+    end
+
+
+
+
 end
--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,9 @@
 class Player
 
+    def get_move
+        puts "enter a position with coordinates separated with a space like '4 7'"
+        input = gets.chomp
+        [input[0].to_i, input[2].to_i]
+    end
+
 end
